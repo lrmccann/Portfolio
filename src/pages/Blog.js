f97f849382393f12f3e282ac449d6efc3e22e577
Logan McCAnn <loganrmccann@gmail.com>
2021-03-06 15:27:25 -0600

    responsive blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -14,12 +14,12 @@ export default function Blog() {
 
   return (
     <div className="blogRightSide container-fixed">
-      <h1>Blog</h1>
-      <h2>March</h2>
+      <h1 id="blog-header">Blog</h1>
+      <h2 id="month-header">March</h2>
       {(blogsToMap.map((item, myKey) => (
-        <ul>
-          <li key={myKey}><a href={item.link}>{item.title}</a>
-            <p className="blogDate">({item.monthCreated})</p>
+        <ul id="blog-list">
+          <li id="blog-item" key={myKey}><a href={item.link}>{item.title}</a>
+            <p id="blog-date">({item.monthCreated})</p>
           </li>
         </ul>
       )))}
