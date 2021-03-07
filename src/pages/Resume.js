d9e1b80f1b36f5c9ab4c59693f394b6da68dedf4
Logan McCAnn <loganrmccann@gmail.com>
2021-03-06 22:23:16 -0600

    adding style for screens starting at 600px up to 991px

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -9,56 +9,32 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 const resumeDownload = require('../page-images/logan-resume-with-links.pdf');
 
 export default function Resume() {
-    const [screenWidth , setScreenWidth] = useState(window.innerWidth)
-    // let screenWidth = window.screen.width;
-    useEffect(() => {
-        setScreenWidth(window.innerWidth)
-    }, [window.innerWidth])
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const [zoom, setZoomScale] = useState(1.50);
 
-    console.log(screenWidth)
-    if (screenWidth <=  599){
-        return (
-            <div className='resumeCont container-fixed'>
-                <h1 id="resume-header">Resume</h1>
-                <Worker workerUrl='https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.worker.min.js'>
-                    <a className="downloadBtn" target='blank' href={resumeDownload} download='../page-images/logan-resume-with-links.pdf'>
-                        <FontAwesomeIcon className="downloadIcon btn-md" icon={faDownload} />
-                    </a>
-                    <div id="pdfviewer" className="pdfviewer">
-                        <Viewer defaultScale={.58} fileUrl={resume} />
-                    </div>
-                </Worker>
-            </div>
-        )
-    }
-     else if(screenWidth >= 600 && screenWidth <= 992 ){
-        return (
-            <div className='resumeCont container-fixed'>
-                <h1 id="resume-header">Resume</h1>
-                <Worker workerUrl='https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.worker.min.js'>
-                    <a className="downloadBtn" target='blank' href={resumeDownload} download='../page-images/logan-resume-with-links.pdf'>
-                        <FontAwesomeIcon className="downloadIcon btn-md" icon={faDownload} />
-                    </a>
-                    <div id="pdfviewer" className="pdfviewer">
-                        <Viewer defaultScale={1.10} fileUrl={resume} />
-                    </div>
-                </Worker>
-            </div>
-        )
-    }
-    else{
-        return (
-            <div className='resumeCont container-fixed'>
-                <h1 id="resume-header">Resume</h1>
-                <Worker workerUrl='https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.worker.min.js'>
-                    <a className="downloadBtn" target='blank' href={resumeDownload} download='../page-images/logan-resume-with-links.pdf'>
-                        <FontAwesomeIcon className="downloadIcon btn-md" icon={faDownload} />
-                    </a>
-                    <div id="pdfviewer" className="pdfviewer">
-                        <Viewer defaultScale={1.50} fileUrl={resume} />
-                    </div>
-                </Worker>
-            </div>
-        )
-    }
+
+    useEffect(() => {
+        if (screenWidth <= 599) {
+            setZoomScale(.59)
+        }
+        else if (screenWidth >= 600 && screenWidth <= 991) {
+            setZoomScale(1)
+        } else {
+            setZoomScale(1.50)
+        }
+    })
+
+    return (
+        <div className='resumeCont container-fixed'>
+            <h1 id="resume-header">Resume</h1>
+            <Worker workerUrl='https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.worker.min.js'>
+                <a className="downloadBtn" target='blank' href={resumeDownload} download='../page-images/logan-resume-with-links.pdf'>
+                    <FontAwesomeIcon className="downloadIcon btn-md" icon={faDownload} />
+                </a>
+                <div id="pdfviewer" className="pdfviewer">
+                    <Viewer defaultScale={zoom} fileUrl={resume} />
+                </div>
+            </Worker>
+        </div>
+    )
 }
