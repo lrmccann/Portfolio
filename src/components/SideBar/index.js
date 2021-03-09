852159c0b1f105e3fe9537074afec27a140b1fc8
Logan McCAnn <loganrmccann@gmail.com>
2021-03-08 18:57:22 -0600

    Cleaning code for side bar component, renaming html & css els from camel case to kebab case

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -12,114 +12,126 @@ export default function SideBar(props) {
     const homePage = () => {
         history.push('/home');
     }
-    const [homeNavLink, setHomeNavLink] = useState(<FontAwesomeIcon id="fontAwsIcon" icon={faHome} />)
+    const [homeNavLink, setHomeNavLink] = useState(<FontAwesomeIcon id="font-aws-icon" icon={faHome} />)
     const hoverHomeLink = e => {
         e.preventDefault();
-        setHomeNavLink(<h5 id="textAfterFlip">Home</h5>)
+        setHomeNavLink(<h5 id="navbar-text">Home</h5>)
     }
     const resetHomeLink = e => {
         e.preventDefault();
-        setHomeNavLink(<FontAwesomeIcon id="fontAwsIcon" icon={faHome} />)
+        setHomeNavLink(<FontAwesomeIcon id="font-aws-icon" icon={faHome} />)
     }
     //
     // PROJECTS BTN FUNC
     const projectPage = () => {
         history.push('/projects');
     }
-    const [projectsNavLink, setprojectNavLink] = useState(<FontAwesomeIcon id="fontAwsIcon" icon={faFolder} />)
+    const [projectNavLink, setprojectNavLink] = useState(<FontAwesomeIcon id="font-aws-icon" icon={faFolder} />)
     const hoverProjectLink = e => {
         e.preventDefault();
-        setprojectNavLink(<h5 id="textAfterFlip">Projects</h5>)
+        setprojectNavLink(<h5 id="navbar-text">Projects</h5>)
     }
     const resetProjectLink = e => {
         e.preventDefault();
-        setprojectNavLink(<FontAwesomeIcon id="fontAwsIcon" icon={faFolder} />)
+        setprojectNavLink(<FontAwesomeIcon id="font-aws-icon" icon={faFolder} />)
     }
     //
     // RESUME BTN FUNC
     const resumePage = () => {
         history.push('/resume');
     }
-    const [resumeNavLink, setResumeNavLink] = useState(<FontAwesomeIcon id="fontAwsIcon" icon={faFileAlt} />)
+    const [resumeNavLink, setResumeNavLink] = useState(<FontAwesomeIcon id="font-aws-icon" icon={faFileAlt} />)
     const hoverResumeLink = e => {
         e.preventDefault();
-        setResumeNavLink(<h5 id="textAfterFlip">Resume</h5>)
+        setResumeNavLink(<h5 id="navbar-text">Resume</h5>)
     }
     const resetResumeLink = e => {
         e.preventDefault();
-        setResumeNavLink(<FontAwesomeIcon id="fontAwsIcon" icon={faFileAlt} />)
+        setResumeNavLink(<FontAwesomeIcon id="font-aws-icon" icon={faFileAlt} />)
     }
     //
     // BLOG BTN FUNC
     const blogPage = () => {
         history.push('/blog');
     }
-    const [blogNavLink, setBlogNavLink] = useState(<FontAwesomeIcon id="fontAwsIcon" icon={faBold} />)
+    const [blogNavLink, setBlogNavLink] = useState(<FontAwesomeIcon id="font-aws-icon" icon={faBold} />)
     const hoverBlogLink = e => {
         e.preventDefault();
-        setBlogNavLink(<h5 id="textAfterFlip">Blog</h5>)
+        setBlogNavLink(<h5 id="navbar-text">Blog</h5>)
     }
     const resetBlogLink = e => {
         e.preventDefault();
-        setBlogNavLink(<FontAwesomeIcon id="fontAwsIcon" icon={faBold} />)
+        setBlogNavLink(<FontAwesomeIcon id="font-aws-icon" icon={faBold} />)
     }
     //
     // CONTACT BTN FUNC
     const contactPage = () => {
         history.push('/contact');
     }
-    const [contactNavLink, setContactNavLink] = useState(<FontAwesomeIcon id="fontAwsIcon" icon={faAddressBook} />)
+    const [contactNavLink, setContactNavLink] = useState(<FontAwesomeIcon id="font-aws-icon" icon={faAddressBook} />)
     const hoverContactLink = e => {
         e.preventDefault();
-        setContactNavLink(<h5 id="textAfterFlip">Contact</h5>)
+        setContactNavLink(<h5 id="navbar-text">Contact</h5>)
     }
     const resetContactLink = e => {
         e.preventDefault();
-        setContactNavLink(<FontAwesomeIcon id="fontAwsIcon" icon={faAddressBook} />)
+        setContactNavLink(<FontAwesomeIcon id="font-aws-icon" icon={faAddressBook} />)
     }
     //
 
     return (
-        <div className="sideBarCont container-fixed">
-            <div className="myInfoDiv">
-                <img
-                    alt="Logan McCann"
-                    src={require("../../page-images/canva-logo-2.png")}
-                ></img>
+        <div className="navbar-container container-fixed">
+            <div className="icon-cont">
+                <img alt="Logan McCann" src={require("../../page-images/canva-logo-2.png")}></img>
             </div>
-            <div className="btnCont">
-                <button className="homeBtn" id="navBarBtn" onMouseEnter={hoverHomeLink} onMouseLeave={resetHomeLink} onClick={homePage}>
+            <div className="nav-link-container">
+                <button id="navbar-btn"
+                    onMouseEnter={hoverHomeLink}
+                    onMouseLeave={resetHomeLink}
+                    onClick={homePage}>
                     {homeNavLink}
                 </button>
-                <button className="projectsBtn" id="navBarBtn" onMouseEnter={hoverProjectLink} onMouseLeave={resetProjectLink} onClick={projectPage}>
-                    {projectsNavLink}
+                <button id="navbar-btn"
+                    onMouseEnter={hoverProjectLink}
+                    onMouseLeave={resetProjectLink}
+                    onClick={projectPage}>
+                    {projectNavLink}
                 </button>
-                <button className="resumeBtn" id="navBarBtn" onMouseEnter={hoverResumeLink} onMouseLeave={resetResumeLink} onClick={resumePage}>
+                <button id="navbar-btn"
+                    onMouseEnter={hoverResumeLink}
+                    onMouseLeave={resetResumeLink}
+                    onClick={resumePage}>
                     {resumeNavLink}
                 </button>
-                <button className="blogBtn" id="navBarBtn" onMouseEnter={hoverBlogLink} onMouseLeave={resetBlogLink} onClick={blogPage}>
+                <button id="navbar-btn"
+                    onMouseEnter={hoverBlogLink}
+                    onMouseLeave={resetBlogLink}
+                    onClick={blogPage}>
                     {blogNavLink}
                 </button>
-                <button className="contactBtn" id="navBarBtn" onMouseEnter={hoverContactLink} onMouseLeave={resetContactLink} onClick={contactPage}>
+                <button id="navbar-btn"
+                    onMouseEnter={hoverContactLink}
+                    onMouseLeave={resetContactLink}
+                    onClick={contactPage}>
                     {contactNavLink}
                 </button>
             </div>
-            <div className="socialMediaLinks">
+            <div className="social-media-link-container">
                 <div id="social-media-btn" className="myGithubBtn">
-                    <a id="social-media-a-tag" href="https://github.com/lrmccann" target="_blank" rel="noopener noreferrer">
-                        <img id="social-media-icon" alt="Github Link" 
-                        src={require('../../page-images/github-mini-img.png')}
+                    <a id="social-media-link" href="https://github.com/lrmccann" target="_blank" rel="noopener noreferrer">
+                        <img id="social-media-icon" alt="Github Link"
+                            src={require('../../page-images/github-mini-img.png')}
                         // src={githubImg}
                         ></img>
                     </a>
                 </div>
                 <div id="social-media-btn" className="myMediumBtn">
-                    <a id="social-media-a-tag" href="https://medium.com/@loganrmccann" target="_blank" rel="noopener noreferrer">
+                    <a id="social-media-link" href="https://medium.com/@loganrmccann" target="_blank" rel="noopener noreferrer">
                         <img id="social-media-icon" alt="Medium Link" src={require('../../page-images/medium-icon2.png')}></img>
                     </a>
                 </div>
                 <div id="social-media-btn" className="mylinkedInBtn">
-                    <a id="social-media-a-tag" href="https://www.linkedin.com/in/logan-mccann-381855155/" target="_blank" rel="noopener noreferrer">
+                    <a id="social-media-link" href="https://www.linkedin.com/in/logan-mccann-381855155/" target="_blank" rel="noopener noreferrer">
                         <img id="social-media-icon" alt="LinkedIn Link" src={require('../../page-images/linkedin-icon2.png')}></img>
                     </a>
                 </div>
